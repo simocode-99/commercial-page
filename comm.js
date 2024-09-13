f4bcf065cee438c824a5867f525dfaa8a787bf2e
user.email <simoaraja17@gmail.com>
2024-09-13 23:17:05 +0100

    Adding html, css and JS Files

---
+++ comm.js
@@ -0,0 +1,14 @@
+const myLink = document.getElementById('mylink');
+var listesContent = document.getElementById('listes-content');
+const lastChilds = document.querySelector('last-childs');
+let isVisible = false;
+
+myLink.addEventListener('click', event => {
+    event.preventDefault();
+    if(listesContent.style.display === 'none' || listesContent.style.display === ''){
+        listesContent.style.display = 'block';
+    }
+    else{
+        listesContent.style.display = 'none';
+    }
+})
